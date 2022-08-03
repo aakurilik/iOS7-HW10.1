47726555cabafd351fc7376e73d0a308a2563c7b
Anatoly <njk21@yandex.ru>
2022-08-03 22:06:04 +0300

    Initial Commit

---
+++ iOS7-HW10.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS7-HW10.1
+//
+//  Created by Anatoly Kurilik on 3.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
